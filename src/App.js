dac2db606963c69f8224ae13d8d04066931c5192
Sunny Chanthavong <sudochanth@gmail.com>
2019-03-18 14:40:48 -0600

    footer edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Header';
 import Nav from './Nav';
 import Home from './Home';
 import About from './About';
-import Footer from './Footer';
 import { Switch, Route } from 'react-router-dom';
 import FilmList from './FilmList';
 import './App.css';
@@ -18,7 +17,6 @@ const App = () => {
                 <Route exact path='/About' component={About} />
                 <Route path='/FilmList' component={FilmList} />
             </Switch>
-            <Footer />
         </div>
     );
 };
